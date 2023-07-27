ec78171909cf71ea8def4678cf24d93c88e5ee1d
AkashPulluru <akashpulluru@gmail.com>
2023-07-27 14:40:05 -0400

    second commit

--- associations/app/models/enrollment.rb
+++ associations/app/models/enrollment.rb
@@ -1,9 +1,16 @@
 class Enrollment < ApplicationRecord
 
     has_many(
-        :students.
+        :students,
         class_name: 'User' 
-        foreign_key: 
+        foreign_key: :enrollment_id
+        primary_key: :id 
+    )
 
+    has_many(
+        :courses,
+        class_name: 'Course' 
+        foreign_key: :enrollment_id
+        primary_key: :id 
     )
 end
